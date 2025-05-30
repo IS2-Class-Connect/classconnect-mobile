722cb5593552854e4d252aadab9476ef02ad0976
Sol Urbano <surbano@fi.uba.ar>
2025-05-29 23:07:38 -0300

    feat:  google login works correctly.

--- firebase/GoogleAuth.ts
+++ firebase/GoogleAuth.ts
@@ -1,66 +1,59 @@
-import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+  isErrorWithCode,isSuccessResponse
+} from '@react-native-google-signin/google-signin';
 import { useEffect, useState } from 'react';
 
 // Configure Google Sign-In
-GoogleSignin.configure({
-		webClientId:'737983419302-r38ianoq87iidmnmg46jpugajg1tbebh.apps.googleusercontent.com',
-});
 
+GoogleSignin.configure({
+  webClientId: '278336937854-pots3anfn9ops568409sn276v4moqnre.apps.googleusercontent.com', // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+  scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  hostedDomain: '', // specifies a hosted domain restriction
+  forceCodeForRefreshToken: false, // [Android] related to `serverAuthCode`, read the docs link below *.
+  accountName: '', // [Android] specifies an account name on the device that should be used
+  iosClientId: '278336937854-gfts1of7sasqfio7uk1hjmi62br3e05v.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. "GoogleService-Info-Staging"
+  openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+  profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+});
 const GoogleAuth = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    signIn();
-  }, []);
-
-  const checkCurrentUser = async () => {
-    try {
-      const isSignedIn = await GoogleSignin.isSignedIn();
-      if (isSignedIn) {
-        getCurrentUser();
-      }
-    } catch (error) {
-      console.error('Check user error:', error);
-    }
-  };
-
-  const getCurrentUser = async () => {
-    try {
-      const currentUser = await GoogleSignin.getCurrentUser();
-      setUser(currentUser);
-    } catch (error) {
-      console.error('Get current user error:', error);
-    }
-  };
-
+  // Somewhere in your code
   const signIn = async () => {
     try {
-      setLoading(true);
-      setError(null);
-      
       await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      //setUser(userInfo);
-      console.log(userInfo)
-      // Get user's ID token
-      const { accessToken, idToken } = await GoogleSignin.getTokens();
-      // Send these tokens to your backend for verification
-      
-    } catch (error) {
-      setError(error.message);
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log('Sign in cancelled');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log('Sign in in progress');
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log('Play services not available');
+      const response = await GoogleSignin.signIn();
+      console.log(response)
+      if (isSuccessResponse(response)) {
+       // setState({ userInfo: response.data });
       } else {
-        console.error('Sign-in error:', error);
+        // sign in was cancelled by user
       }
-    } finally {
-      setLoading(false);
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            console.log("in progress")
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+              console.log("PLAY_SERVICES_NOT_AVAILABLE")
+            // Android only, play services not available or outdated
+            break;
+          default:
+            console.log("eerror", error)
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
     }
   };
 
